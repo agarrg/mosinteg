63d4116d0ff6d81533c138b8a377a49566a4e439
agarrg <130370313+agarrg@users.noreply.github.com>
2023-11-20 21:08:07 +0600

    Add files via upload

---
+++ main.go
@@ -0,0 +1,12 @@
+// mosint v2.3
+// Author: Alp Keskin
+// Github: github.com/alpkeskin
+// Linkedin: linkedin.com/in/alpkeskin
+
+package main
+
+import "github.com/alpkeskin/mosint/cmd"
+
+func main() {
+	cmd.Execute()
+}
